c21c953ebe26c84d44e3b8e44d2212c087afbc91
Doncho Minkov <DonchoMinkov@gmail.com>
2020-12-03 19:56:11 +0200

    Added django_heroku lib

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -14,6 +14,8 @@ from os.path import join
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -161,3 +163,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
